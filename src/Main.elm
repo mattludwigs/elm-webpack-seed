572767304d045213ee3828537b3501860f33a499
Matthew Ludwigs <matthew.ludwigs@colonyamerican.com>
2016-05-07 10:47:10 -0700

    update for start app

--- src/Main.elm
+++ src/Main.elm
@@ -1,7 +1,57 @@
 module Main where
 
 import Html exposing (..)
+import Effects exposing (Effects, Never)
+import Task
+import StartApp
 
-main : Html
+-- ACTIONS
+
+type Action 
+  = NoOp
+
+-- MODEL
+
+type alias Model =
+  {}
+
+initialModel : Model
+initialModel =
+  {}
+
+-- UPDATE
+
+update : Action -> Model -> ( Model, Effects Action )
+update action model =
+  ( model, Effects.none )
+
+
+-- VIEW
+view : Signal.Address Action -> Model -> Html
+view address model =
+  div
+    []
+    [ text "Hello" ]
+
+-- START APP 
+
+init : ( Model, Effects Action )
+init =
+  ( initialModel, Effects.none )
+
+app : StartApp.App Model
+app =
+  StartApp.start
+    { init = init
+    , inputs = []
+    , update = update
+    , view = view
+    }
+
+main : Signal Html
 main =
-  text "Hello"
+  app.html
+
+port runner : Signal (Task.Task Never ())
+port runner =
+  app.tasks
